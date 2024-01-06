377689df2cab38186e032af77e8de99c3653f0cb
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-01-06 11:26:08 +0100

    Configured serving static content and added test image

--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = require("../server/routers/wall-router");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 let corsOrigin = {
   origin: process.env.FRONTEND_URL,
 };
-
+const PORT = process.env.PORT;
 const app = express();
-
+app.use(express.static(path.join(__dirname, "..") + "/public"));
 app.use(cors(corsOrigin));
 
 app.use(router);
@@ -17,6 +18,6 @@ app.get("/", (req, res) => {
   res.json({ msg: "This is CORS-enabled for a Single Route" });
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
