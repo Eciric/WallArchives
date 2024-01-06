5549e5a139fb45a8bbe00230797d6f87bd618890
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-01-06 12:25:31 +0100

    Added test image for home creation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { WallService } from '../services/wall.service';
+import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
-  constructor(private http: HttpClient) {}
+export class HomeComponent implements OnInit {
+  imageURL!: unknown;
+
+  constructor(
+    private wallService: WallService,
+    private sanitizer: DomSanitizer
+  ) {}
+
+  ngOnInit(): void {
+    this.wallService.fetchTestImage().subscribe({
+      next: (blob) => {
+        this.imageURL = URL.createObjectURL(blob);
+      },
+      error: (e) => {
+        console.error(e);
+      },
+      complete: () => {
+        console.log('Completed');
+      },
+    });
+  }
 }
