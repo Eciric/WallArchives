396e407d1d9a65dbf4c0df8cded454c0494f7b4b
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-01-08 20:54:53 +0100

    Now displaying all images with styling changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,29 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { WallService } from '../services/wall.service';
-import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  imageURL!: unknown;
-
-  constructor(
-    private wallService: WallService,
-    private sanitizer: DomSanitizer
-  ) {}
+  imageURLS!: string[];
+  api = 'http://localhost:3002/uploads';
+  constructor(private wallService: WallService) {}
 
   ngOnInit(): void {
-    this.wallService.fetchTestImage().subscribe({
-      next: (blob) => {
-        this.imageURL = URL.createObjectURL(blob);
-      },
-      error: (e) => {
-        console.error(e);
-      },
-      complete: () => {
-        console.log('Completed');
+    this.wallService.fetchAllImages().subscribe({
+      next: (imageURLS) => {
+        this.imageURLS = imageURLS;
+        console.log(this.imageURLS);
       },
     });
   }
