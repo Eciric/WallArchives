af7a356b919196e8456c6df867d16dcd8a951867
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-01-08 20:54:20 +0100

    Added endpoint to fetch all image names

--- server/routers/wall-router.js
+++ server/routers/wall-router.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const router = express.Router();
 
-router.get("/wall/:id", (req, res) => {
-  res.status(200).send(JSON.stringify(req.params));
+const uploadsFolder = `${path.join(__dirname, "..", "..")}/public/uploads`;
+
+router.get("/walls", (req, res) => {
+  const fileNames = fs.readdirSync(uploadsFolder);
+  res.status(200).send(fileNames);
+});
+
+router.get("/walls/:id", (req, res) => {
+  res.status(200).send("test" + id + ".png");
 });
 
 module.exports = router;
