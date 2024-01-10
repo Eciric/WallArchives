34025ca7bd7acd88295037d0e56fc618717c464e
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-01-10 15:59:34 +0100

    Bound form controls to the formGroup in sign up component

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/interfaces/user';
+import { UserResponse } from 'src/app/interfaces/user-response';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['./sign-up.component.css'],
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+  formGroup!: FormGroup;
+  user: User = {} as User;
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.formGroup = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      username: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(16),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+    });
+  }
+
+  createNewUser(): void {
+    this.userService
+      .createUser({
+        email: this.formGroup.get('email')?.value,
+        username: this.formGroup.get('username')?.value,
+        password: this.formGroup.get('password')?.value,
+      })
+      .subscribe({
+        next: (response: UserResponse) => {
+          console.log(response);
+        },
+        error: (e: Error) => {
+          console.log(e);
+        },
+        complete: () => {
+          console.log('user creation call completed');
+        },
+      });
+  }
 }
