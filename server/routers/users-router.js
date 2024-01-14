5ea79f74d4a247fe81a0e84d8495fbe7f84d0fa0
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-01-14 14:24:26 +0100

    User authentication using mongoose and passport

---
+++ server/routers/users-router.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../../models/User");
+const passport = require("passport");
+
+passport.use(User.createStrategy());
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
+
+router.post("/users", async (req, res) => {
+  try {
+    const registerUser = await User.register(
+      {
+        email: req.body.email,
+        username: req.body.username,
+        dateCreated: Date.now(),
+      },
+      req.body.password
+    );
+    if (registerUser) {
+      passport.authenticate("local")(req, res, () => {
+        res.status(200).send(req.body);
+      });
+    } else {
+      res.status(500).send(req.body);
+    }
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
+});
+
+router.post("/login", (req, res) => {
+  const user = new User({
+    email: req.body.email,
+    passwowrd: req.body.email,
+  });
+
+  req.login(user, (err) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send();
+    } else {
+      passport.authenticate("local")(req, res, () => {
+        res.status(200).send();
+      });
+    }
+  });
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.status(200).send();
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,23 +1,50 @@
+require("dotenv").config();
 const express = require("express");
-const router = require("../server/routers/wall-router");
 const cors = require("cors");
 const path = require("path");
-require("dotenv").config();
+const wallsRouter = require("../server/routers/walls-router");
+const usersRouter = require("../server/routers/users-router");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
 
-let corsOrigin = {
-  origin: process.env.FRONTEND_URL,
-};
 const PORT = process.env.PORT;
+
 const app = express();
 
-app.use(cors(corsOrigin));
-
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+  })
+);
 app.use(express.static(path.join(__dirname, "..") + "/public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(router);
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get("/", (req, res) => {
-  res.json({ msg: "This is  a  Route" });
+mongoose
+  .connect(process.env.ATLAS_URI)
+  .then(() => {
+    console.log("database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(wallsRouter);
+app.use(usersRouter);
+
+app.get("/", (_, res) => {
+  res.json({ msg: "Valid routes: /walls, /users" });
 });
 
 app.listen(PORT, () => {
