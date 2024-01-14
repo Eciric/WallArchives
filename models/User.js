2330f33b93c57f842293fe98d8973ede644b4de2
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-01-14 15:02:57 +0100

    User model added required fields

--- models/User.js
+++ models/User.js
@@ -3,10 +3,10 @@ const passportLM = require("passport-local-mongoose");
 
 //User schema
 const userSchema = new mongoose.Schema({
-  email: String,
-  username: String,
-  password: String,
-  dateCreated: Date,
+  email: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  dateCreated: { type: Date, default: Date.now },
 });
 
 userSchema.plugin(passportLM);
