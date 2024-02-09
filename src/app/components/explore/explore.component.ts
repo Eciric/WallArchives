4acfb66cf6d720443b174581cce073c96d836809
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-02-09 18:40:17 +0100

    Added api url from environment to components

--- src/app/components/explore/explore.component.ts
+++ src/app/components/explore/explore.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Wall } from 'src/app/interfaces/wall';
+import { WallService } from 'src/app/services/wall/wall.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-explore',
   templateUrl: './explore.component.html',
-  styleUrls: ['./explore.component.css']
+  styleUrls: ['./explore.component.css'],
 })
 export class ExploreComponent {
+  images: Wall[] = [];
+  api: string = environment.apiUrl + '/uploads';
 
+  constructor(private wallService: WallService) {}
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,23 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { WallService } from '../../services/wall/wall.service';
 import { Wall } from 'src/app/interfaces/wall';
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  imageURLS!: string[];
-  api = 'http://localhost:3002/uploads';
+  images!: Wall[];
+  api = environment.apiUrl + '/uploads';
 
   constructor(private wallService: WallService) {}
 
   ngOnInit(): void {
     this.wallService.fetchAllImages().subscribe({
       next: (images: Wall[]) => {
-        this.imageURLS = images.map((image) => {
-          return image.path;
-        });
+        this.images = images;
       },
     });
   }
