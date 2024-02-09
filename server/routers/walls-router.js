4e002b8856cef89236ba0900ddc343e399183603
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-02-09 12:53:59 +0100

    Modified /walls endpoint to fetch data from DB

--- server/routers/walls-router.js
+++ server/routers/walls-router.js
@@ -2,12 +2,18 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const router = express.Router();
+const Wall = require("../../models/Wall");
 
 const uploadsFolder = `${path.join(__dirname, "..", "..")}/public/uploads`;
 
-router.get("/walls", (req, res) => {
-  const fileNames = fs.readdirSync(uploadsFolder);
-  res.status(200).send(fileNames);
+router.get("/walls", async (req, res) => {
+  const walls = await Wall.find();
+  console.log(walls);
+  if (walls) {
+    res.status(200).send(walls);
+  } else {
+    res.status(400).send();
+  }
 });
 
 router.get("/walls/:id", (req, res) => {
