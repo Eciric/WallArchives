6125e698f82cf588d128c3c04680b1456722c9b3
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-02-11 10:48:15 +0100

    Added http unit tests for wallService

--- src/app/services/wall/wall.service.spec.ts
+++ src/app/services/wall/wall.service.spec.ts
@@ -1,17 +1,57 @@
 import { TestBed } from '@angular/core/testing';
-
+import { createSpyFromClass, Spy } from 'jasmine-auto-spies';
 import { WallService } from './wall.service';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { Wall } from 'src/app/interfaces/wall';
+import { isEqual } from 'lodash';
 
 describe('WallService', () => {
   let service: WallService;
+  let httpSpy: Spy<HttpClient>;
+
+  let fakeWalls: Wall[] = [
+    {
+      _id: '65c3f07e1b69e02d19a415fb',
+      _uid: '65c3f07e1b69e02d19a415fb',
+      title: 'A test wallpaper',
+      path: '/test.png',
+      tags: 'sky, tree, forest, urban, colorful',
+      date: new Date(),
+    },
+    {
+      _id: '65c3f07e1b69e02d19a415fc',
+      _uid: '65c3f07e1b69e02d19a415fc',
+      title: 'A test wallpaper 1',
+      path: '/test1.png',
+      tags: 'sky, tree, forest, urban, colorful',
+      date: new Date(),
+    },
+  ];
 
   beforeEach(() => {
-    TestBed.configureTestingModule({ imports: [HttpClientModule] });
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: HttpClient, useValue: createSpyFromClass(HttpClient) },
+      ],
+    });
     service = TestBed.inject(WallService);
+    httpSpy = TestBed.inject<any>(HttpClient);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return the expected wallpapers', () => {
+    httpSpy.get.and.nextWith(fakeWalls);
+
+    service.fetchAllImages().subscribe((walls) => {
+      expect(walls).toHaveSize(fakeWalls.length);
+      walls.forEach((wall, index) => {
+        expect(isEqual(wall, fakeWalls[index])).toBeTruthy();
+      });
+    });
+
+    expect(httpSpy.get.calls.count()).toBe(1);
   });
 });
