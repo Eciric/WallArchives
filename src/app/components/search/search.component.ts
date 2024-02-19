78b8639b0a6adcebb2473292ac625f9153a4f4a8
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-02-19 16:59:37 +0100

    Search component progress

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Wall } from 'src/app/interfaces/wall';
+import { WallService } from 'src/app/services/wall/wall.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent {
+  @Input() search!: string;
+  @Output() wallEmitter!: EventEmitter<Wall[]>;
 
+  constructor(private wallService: WallService) {}
+
+  onKeydown(event: Event) {
+    event.preventDefault();
+    const target = event.target as HTMLInputElement;
+    this.wallService.getWallsByKeyword(target.value).subscribe({
+      next: (walls: Wall[]) => {
+        this.wallEmitter.emit(walls);
+      },
+      error: (err: any) => {},
+    });
+  }
 }
