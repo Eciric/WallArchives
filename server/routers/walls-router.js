ab388a1d3ad78e95a230dce2a8749bd2ee41ac74
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-02-19 16:58:34 +0100

    Added an endpoint to get all walls by a keyword

--- server/routers/walls-router.js
+++ server/routers/walls-router.js
@@ -1,15 +1,11 @@
 const express = require("express");
-const fs = require("fs");
-const path = require("path");
 const router = express.Router();
 const Wall = require("../../models/Wall");
-
-const uploadsFolder = `${path.join(__dirname, "..", "..")}/public/uploads`;
 
 router.get("/walls", async (req, res) => {
   const walls = await Wall.find();
   if (walls) {
-    res.status(200).send(walls);
+    res.status(200).send(JSON.stringify(walls));
   } else {
     res.status(400).send();
   }
@@ -18,7 +14,19 @@ router.get("/walls", async (req, res) => {
 router.get("/walls/:id", async (req, res) => {
   const wall = await Wall.findById(req.params.id);
   if (wall) {
-    res.status(200).send(wall);
+    res.status(200).send(JSON.stringify(wall));
+  } else {
+    res.status(404).send();
+  }
+});
+
+router.get("/walls/:wallKeyword", async (req, res) => {
+  console.log("getting", req.params.wallKeyword);
+  let filter = { tags: [`${req.params.wallKeyword}`] };
+  filter.tags = { $in: filter.tags.map((t) => new RegExp(t)) };
+  const walls = await Wall.find(filter);
+  if (walls) {
+    res.status(200).send(JSON.stringify(walls));
   } else {
     res.status(404).send();
   }
