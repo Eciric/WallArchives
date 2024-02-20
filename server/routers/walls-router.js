65435dd689530345d8e42296bf900167d32746d9
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-02-20 13:04:37 +0100

    Error handling and fix for the keyword endpoint

--- server/routers/walls-router.js
+++ server/routers/walls-router.js
@@ -3,32 +3,56 @@ const router = express.Router();
 const Wall = require("../../models/Wall");
 
 router.get("/walls", async (req, res) => {
-  const walls = await Wall.find();
-  if (walls) {
-    res.status(200).send(JSON.stringify(walls));
-  } else {
-    res.status(400).send();
+  try {
+    const walls = await Wall.find();
+    if (walls) {
+      res.status(200).json(walls);
+    } else {
+      res.status(400).send();
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
   }
 });
 
 router.get("/walls/:id", async (req, res) => {
-  const wall = await Wall.findById(req.params.id);
-  if (wall) {
-    res.status(200).send(JSON.stringify(wall));
-  } else {
-    res.status(404).send();
+  try {
+    if (req.params.id == "keyword") {
+      res.redirect("/walls/keyword/all");
+      return;
+    }
+    const wall = await Wall.findById(req.params.id);
+    if (wall) {
+      res.status(200).json(wall);
+    } else {
+      res.status(404).send();
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
   }
 });
 
-router.get("/walls/:wallKeyword", async (req, res) => {
-  console.log("getting", req.params.wallKeyword);
-  let filter = { tags: [`${req.params.wallKeyword}`] };
-  filter.tags = { $in: filter.tags.map((t) => new RegExp(t)) };
-  const walls = await Wall.find(filter);
-  if (walls) {
-    res.status(200).send(JSON.stringify(walls));
-  } else {
-    res.status(404).send();
+router.get("/walls/keyword/:keyword", async (req, res) => {
+  try {
+    let walls;
+    if (req.params.keyword == "all") {
+      walls = await Wall.find();
+    } else {
+      walls = await Wall.find({
+        tags: { $in: req.params.keyword },
+      });
+    }
+
+    if (walls) {
+      res.status(200).json(walls);
+    } else {
+      res.status(404).send("Resource not found");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
   }
 });
 
