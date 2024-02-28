fa5df236400de42fb9dbd3b5532f679d94a5e9b8
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-02-28 14:29:39 +0100

    User profile now shows walls added by user

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,7 +1,10 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserResponse } from 'src/app/interfaces/user-response';
+import { Wall } from 'src/app/interfaces/wall';
+import { WallResponse } from 'src/app/interfaces/wall-response';
 import { UserService } from 'src/app/services/user/user.service';
+import { WallService } from 'src/app/services/wall/wall.service';
 
 @Component({
   selector: 'app-profile',
@@ -10,13 +13,18 @@ import { UserService } from 'src/app/services/user/user.service';
 })
 export class ProfileComponent implements OnInit {
   user: UserResponse = {} as UserResponse;
+  userWallResponse!: WallResponse;
   currentControl = 'info';
   controls = [
     { value: 'info', name: 'my info', state: true },
     { value: 'wall', name: 'my wallpapers', state: false },
   ];
 
-  constructor(private userService: UserService, private router: Router) {}
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private wallService: WallService
+  ) {}
 
   ngOnInit(): void {
     this.user = this.userService.getUserInfo() || ({} as UserResponse);
@@ -26,5 +34,21 @@ export class ProfileComponent implements OnInit {
     this.controls.map((control) => (control.state = false));
     this.controls[emitted].state = true;
     this.currentControl = this.controls[emitted].value;
+    if (this.currentControl == 'wall') {
+      this.fetchUserWalls();
+    }
+  }
+
+  fetchUserWalls(): void {
+    this.wallService.getWallsByUID(this.user.uid).subscribe({
+      next: (wallResponse: WallResponse) => {
+        if (wallResponse) {
+          this.userWallResponse = wallResponse;
+        }
+      },
+      error: (error: any) => {
+        console.error(error);
+      },
+    });
   }
 }
